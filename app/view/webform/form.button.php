48e2a1982bf9cded101ee904ec9ccef78159a4d8
HENRY SHUM <henrygotmojo@gmail.com>
2021-06-29 21:46:48 +0800

    apply update button

--- app/view/webform/form.button.php
+++ app/view/webform/form.button.php
@@ -4,6 +4,7 @@
 		<in>
 			<structure name="$xfa">
 				<string name="submit" optional="yes" />
+				<string name="update" optional="yes" />
 				<string name="back" optional="yes" />
 				<string name="next" optional="yes" />
 			</structure>
@@ -13,20 +14,28 @@
 </fusedoc>
 */
 ?><div id="webform-form-button" class="form-group mt-5"><?php
-	if ( isset($xfa['submit']) ) :
+	if ( isset($xfa['submit']) or isset($xfa['update']) ) :
 		?><div class="text-center"><?php
 			// submit button
 			if ( isset($xfa['submit']) ) :
 				?><button 
 					type="submit"
+					class="btn btn-lg btn-primary btn-update mx-1"
 					formaction="<?php echo F::url($xfa['submit']); ?>"
-					class="btn btn-lg btn-primary mx-1"
-				><?php echo ( Webform::mode() == 'edit' ) ? 'Update' : 'Submit'; ?></button><?php
+				>Submit</button><?php
+			endif;
+			// update button
+			if ( isset($xfa['update']) ) :
+				?><button 
+					type="submit"
+					class="btn btn-lg btn-primary btn-submit mx-1"
+					formaction="<?php echo F::url($xfa['update']); ?>"
+				>Update</button><?php
 			endif;
 		?></div><?php
 	endif;
 	// separator
-	if ( isset($xfa['submit']) and ( isset($xfa['back']) or isset($xfa['next']) ) ) :
+	if ( ( isset($xfa['submit']) or isset($xfa['update']) ) and ( isset($xfa['back']) or isset($xfa['next']) ) ) :
 		?><hr class="mt-5 mb-4" /><?php
 	endif;
 	if ( isset($xfa['back']) or isset($xfa['next']) ) :
@@ -34,16 +43,16 @@
 			// back button
 			if ( isset($xfa['back']) ) :
 				?><a 
+					class="btn btn-light btn-back float-left"
 					href="<?php echo F::url($xfa['back']); ?>"
-					class="btn btn-light float-left"
 				><i class="fa fa-arrow-left"></i> Back</a><?php
 			endif;
 			// next button
 			if ( isset($xfa['next']) ) :
 				?><button
 					type="submit"
+					class="btn btn-primary btn-next float-right"
 					formaction="<?php echo F::url($xfa['next']); ?>"
-					class="btn btn-primary float-right"
 				>Next <i class="fa fa-arrow-right ml-1"></i></button><?php
 			endif;
 		?></div><?php
