b8e0c99f53f01ab8fa1c175aecdb95fb692918b3
HENRY SHUM <henrygotmojo@gmail.com>
2021-06-29 21:59:08 +0800

    add update button and add icons

--- app/view/webform/form.button.php
+++ app/view/webform/form.button.php
@@ -3,10 +3,11 @@
 	<io>
 		<in>
 			<structure name="$xfa">
-				<string name="submit" optional="yes" />
-				<string name="update" optional="yes" />
 				<string name="back" optional="yes" />
 				<string name="next" optional="yes" />
+				<string name="edit" optional="yes" />
+				<string name="submit" optional="yes" />
+				<string name="update" optional="yes" />
 			</structure>
 		</in>
 		<out />
@@ -14,7 +15,7 @@
 </fusedoc>
 */
 ?><div id="webform-form-button" class="form-group mt-5"><?php
-	if ( isset($xfa['submit']) or isset($xfa['update']) ) :
+	if ( isset($xfa['submit']) or isset($xfa['update']) or isset($xfa['edit']) ) :
 		?><div class="text-center"><?php
 			// submit button
 			if ( isset($xfa['submit']) ) :
@@ -22,7 +23,7 @@
 					type="submit"
 					class="btn btn-lg btn-primary btn-update mx-1"
 					formaction="<?php echo F::url($xfa['submit']); ?>"
-				>Submit</button><?php
+				><i class="fa fa-paper-plane"></i> Submit</button><?php
 			endif;
 			// update button
 			if ( isset($xfa['update']) ) :
@@ -30,12 +31,19 @@
 					type="submit"
 					class="btn btn-lg btn-primary btn-submit mx-1"
 					formaction="<?php echo F::url($xfa['update']); ?>"
-				>Update</button><?php
+				><i class="fa fa-download"></i> Update</button><?php
+			endif;
+			// edit button
+			if ( isset($xfa['edit']) ) :
+				?><a 
+					class="btn btn-lg btn-light btn-edit mx-1"
+					href="<?php echo F::url($xfa['edit']); ?>"
+				><i class="fa fa-pencil-alt"></i> Edit</a><?php
 			endif;
 		?></div><?php
 	endif;
 	// separator
-	if ( ( isset($xfa['submit']) or isset($xfa['update']) ) and ( isset($xfa['back']) or isset($xfa['next']) ) ) :
+	if ( ( isset($xfa['submit']) or isset($xfa['update']) or isset($xfa['edit']) ) and ( isset($xfa['back']) or isset($xfa['next']) ) ) :
 		?><hr class="mt-5 mb-4" /><?php
 	endif;
 	if ( isset($xfa['back']) or isset($xfa['next']) ) :
