ce2288f34686928e62c27999789f694078312b72
henrygotmojo <henrygotmojo@gmail.com>
2021-06-29 18:11:26 +0800

    show captcha in form when submit button exists

--- app/view/webform/form.captcha.php
+++ app/view/webform/form.captcha.php
@@ -1,1 +1,15 @@
-<div id="webform-form-captcha" class="text-center"><h1>[captcha]</h1></div>
+<?php /*
+<fusedoc>
+	<io>
+		<in>
+			<structure name="$xfa">
+				<string name="submit" optional="yes" />
+			</structure>
+		</in>
+		<out />
+	</io>
+</fusedoc>
+*/ ?>
+<div id="webform-form-captcha" class="text-center mt-5"><?php
+	if ( isset($xfa['submit']) and class_exists('Captcha') ) echo Captcha::field();
+?></div>
--- app/view/webform/form.php
+++ app/view/webform/form.php
@@ -16,6 +16,6 @@
 */ ?>
 <form id="webform-form" method="post" class="<?php echo Webform::$config['beanType']; ?>"><?php
 include 'form.body.php';
-if ( isset($xfa['submit']) and class_exists('Captcha') ) include 'form.captcha.php';
+include 'form.captcha.php';
 include 'form.button.php';
 ?></form>
