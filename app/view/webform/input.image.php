3c02bf099564f02d0eb4c01dfdca10ee6ccf9e8c
HENRY SHUM <henrygotmojo@gmail.com>
2021-07-07 21:19:22 +0800

    apply webform-input-image class for image field

--- app/view/webform/input.image.php
+++ app/view/webform/input.image.php
@@ -1,2 +1,11 @@
 <?php
+// capture original output
+ob_start();
 include 'input.file.php';
+$output = ob_get_clean();
+
+// change class name
+$output = str_ireplace('webform-input-file', 'webform-input-image', $output);
+
+// display
+echo $output;
