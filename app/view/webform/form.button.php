067e108f2cde254ef2cef98577de0c8d09b5303b
henrygotmojo <henrygotmojo@gmail.com>
2021-07-07 14:58:10 +0800

    show print button

--- app/view/webform/form.button.php
+++ app/view/webform/form.button.php
@@ -15,15 +15,18 @@
 	</io>
 </fusedoc>
 */
-if ( !empty($xfa['submit']) or !empty($xfa['update']) or !empty($xfa['edit']) or !empty($xfa['print']) or !empty($xfa['back']) or !empty($xfa['next']) ) :
+$hasUpperButton = ( !empty($xfa['submit']) or !empty($xfa['update']) or !empty($xfa['edit']) or !empty($xfa['print']) );
+$hasLowerButton = ( !empty($xfa['back']) or !empty($xfa['next']) );
+if ( $hasUpperButton or $hasLowerButton ) :
 	?><div id="webform-form-button" class="form-group mt-5"><?php
-		if ( !empty($xfa['submit']) or !empty($xfa['update']) or !empty($xfa['edit']) ) :
+		// upper-button
+		if ( $hasUpperButton ) :
 			?><div class="text-center"><?php
 				// submit button
 				if ( !empty($xfa['submit']) ) :
 					?><button 
 						type="submit"
-						class="btn btn-lg btn-primary btn-update mx-1"
+						class="btn btn-lg btn-primary btn-update mx-2"
 						formaction="<?php echo F::url($xfa['submit']); ?>"
 					><i class="fa fa-paper-plane"></i> Submit</button><?php
 				endif;
@@ -31,24 +34,33 @@ if ( !empty($xfa['submit']) or !empty($xfa['update']) or !empty($xfa['edit']) or
 				if ( !empty($xfa['update']) ) :
 					?><button 
 						type="submit"
-						class="btn btn-lg btn-primary btn-submit mx-1"
+						class="btn btn-lg btn-primary btn-submit mx-2"
 						formaction="<?php echo F::url($xfa['update']); ?>"
 					><i class="fa fa-download"></i> Update</button><?php
 				endif;
 				// edit button
 				if ( !empty($xfa['edit']) ) :
 					?><a 
-						class="btn btn-lg btn-dark btn-edit mx-1"
+						class="btn btn-lg btn-dark btn-edit mx-2"
 						href="<?php echo F::url($xfa['edit']); ?>"
 					><i class="fa fa-edit"></i> Edit</a><?php
+				endif;
+				// print button
+				if ( !empty($xfa['print']) ) :
+					?><a 
+						class="btn btn-lg btn-light btn-print border mx-2"
+						href="<?php echo F::url($xfa['print']); ?>"
+						target="_blank"
+					><i class="fa fa-print"></i> Print</a><?php
 				endif;
 			?></div><?php
 		endif;
 		// separator
-		if ( ( !empty($xfa['submit']) or !empty($xfa['update']) or !empty($xfa['edit']) ) and ( !empty($xfa['back']) or !empty($xfa['next']) ) ) :
+		if (  $hasUpperButton and $hasLowerButton ) :
 			?><hr class="mt-5 mb-4" /><?php
 		endif;
-		if ( !empty($xfa['back']) or !empty($xfa['next']) ) :
+		// lower-button
+		if ( $hasLowerButton ) :
 			?><div class="overflow-auto"><?php
 				// back button
 				if ( !empty($xfa['back']) ) :
