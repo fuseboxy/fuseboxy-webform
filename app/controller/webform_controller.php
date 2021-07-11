d68efb2ca2712ffe0ceaee565aedf20ce2240755
henrygotmojo <henrygotmojo@gmail.com>
2021-07-11 16:41:02 +0800

    back to webform-start instead of webform-index (to avoid index page being use for other purposes)

--- app/controller/webform_controller.php
+++ app/controller/webform_controller.php
@@ -168,7 +168,7 @@ switch ( $fusebox->action ) :
 		// exit point : back
 		$prevStep = Webform::prevStep($arguments['step']);
 		if ( $prevStep ) $xfa['back'] = "{$fusebox->controller}.edit&step={$prevStep}";
-		elseif ( $arguments['step'] == $firstStep ) $xfa['back'] = $fusebox->controller;
+		elseif ( $arguments['step'] == $firstStep ) $xfa['back'] = "{$fusebox->controller}.start";
 		// exit point : next
 		$nextStep = Webform::nextStep($arguments['step']);
 		if ( $nextStep ) $xfa['next'] = "{$fusebox->controller}.validate&step={$arguments['step']}";
