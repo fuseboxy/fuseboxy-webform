44b45af5b004e35f6ffb21e4a57038ed71865b57
henrygotmojo <henrygotmojo@gmail.com>
2021-07-13 17:35:10 +0800

    apply icon & inline-label to output format

--- app/view/webform/input.output.php
+++ app/view/webform/input.output.php
@@ -7,16 +7,19 @@
 			<structure name="$fieldConfig">
 				<string name="class" optional="yes" />
 				<string name="style" optional="yes" />
-				<boolean name="readonly" optional="yes" comments="for styling only" />
 			</structure>
 		</in>
 		<out />
 	</io>
 </fusedoc>
 */ ?>
-<div 
-	id="<?php echo $fieldID; ?>"
-	class="webform-input-output <?php if ( !empty($fieldConfig['class']) ) echo $fieldConfig['class']; ?>"
-	<?php if ( !empty($fieldConfig['style']) ) : ?>style="<?php echo $fieldConfig['style']; ?>"<?php endif; ?>
-	<?php if ( !empty($fieldConfig['readonly']) ) echo 'readonly'; ?>
-><?php echo $fieldValue; ?></div>
+<div class="webform-input-output input-group"><?php
+	// icon
+	include F::appPath('view/webform/input.icon.php');
+	// output
+	?><div 
+		id="<?php echo $fieldID; ?>"
+		class="<?php if ( !empty($fieldConfig['class']) ) echo $fieldConfig['class']; ?>"
+		<?php if ( !empty($fieldConfig['style']) ) : ?>style="<?php echo $fieldConfig['style']; ?>"<?php endif; ?>
+	><?php echo $fieldValue; ?></div>
+</div>
