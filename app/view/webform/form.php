9937d693e8554e16520a2453ef1131a227c30e78
henrygotmojo <henrygotmojo@gmail.com>
2021-07-16 14:54:14 +0800

    check available of step when show as form attribute

--- app/view/webform/form.php
+++ app/view/webform/form.php
@@ -13,13 +13,18 @@
 			<structure name="$fieldLayout" optional="yes" comments="display single step">
 				<list name="~fieldNameList~" value="~fieldWidthList~" delim="|" />
 			</structure>
-			<string name="$step" />
+			<string name="$step" optional="yes" />
 		</in>
 		<out />
 	</io>
 </fusedoc>
 */ ?>
-<form id="webform-form" method="post" class="<?php echo Webform::$config['beanType']; ?>" data-step="<?php echo $step; ?>"><?php
+<form 
+	id="webform-form"
+	method="post"
+	class="<?php echo Webform::$config['beanType']; ?>"
+	<?php if ( !empty($step) ) : ?>data-step="<?php echo $step; ?>"<?php endif; ?>
+><?php
 // display single or multiple steps
 if ( isset($fieldLayoutAll) ) foreach ( $fieldLayoutAll as $fieldLayout ) include F::appPath('view/webform/form.body.php');
 elseif ( isset($fieldLayout) ) include F::appPath('view/webform/form.body.php');
