9ab6f758780d42a1a990ec807d61ae3099e3663e
henrygotmojo <henrygotmojo@gmail.com>
2021-08-23 16:27:32 +0800

    create prelim input-table template

---
+++ app/view/webform/input.table.php
@@ -0,0 +1,78 @@
+<?php /*
+<fusedoc>
+	<description>
+		render table which submits array-of-structure
+	</description>
+	<io>
+		<in>
+			<boolean name="$editable" />
+			<string name="$fieldID" />
+			<string name="$fieldName" />
+			<array name="$fieldValue">
+				<structure name="+" />
+			</arrya>
+			<string name="$dataFieldName" />
+			<structure name="$fieldConfig">
+				<string name="tableTitle" optional="yes" />
+				<structure name="tableHeader" optional="yes">
+					<string name="~columnHeader~" value="~columnWidth~" />
+				</structure>
+				<file name="tableRow" optional="yes" example="/path/to/table/row.php" />
+				<boolean name="appendRow" optional="yes" />
+				<boolean name="removeRow" optional="yes" />
+			</structure>
+		</in>
+		<out>
+			<structure name="data" scope="form" optional="yes">
+				<array name="~fieldName~">
+					<structure name="+" />
+				</array>
+			</structure>
+		</out>
+	</io>
+</fusedoc>
+*/ ?>
+<div id="<?php echo $fieldID; ?>" class="webform-input-table input-group">
+	<header>
+		<table class="table table-bordered mb-0">
+			<thead><?php
+				// title
+				if ( !empty($fieldConfig['tableTitle']) ) :
+					?><tr class="bg-light">
+						<th colspan="5" class="bb-0"><?php echo $fieldConfig['tableTitle']; ?></th>
+					</tr><?php
+				endif;
+				?><tr class="text-center bg-white small"><?php
+					// header
+					if ( !empty($fieldConfig['tableHeader']) ) :
+						foreach ( $fieldConfig['tableHeader'] as $headerText => $headerWidth ) :
+							if ( is_numeric($headerText) ) list($headerText, $headerWidth) = [ $headerWidth, '' ];
+							?><th <?php if ( !empty($headerWidth) ) echo "width='{$headerWidth}'"; ?>><?php echo $headerText; ?></th><?php
+						endforeach;
+					endif;
+					// button
+					if ( !empty($fieldConfig['appendRow']) ) :
+						?><th width="5%" class="text-center px-0"><?php
+							?><a 
+								href="<?php echo $fieldConfig['appendRow'] ?>"
+								class="btn btn-sm btn-success"
+								data-toggle="ajax-load"
+								data-target="#<?php echo $fieldID; ?> > fieldset"
+								data-mode="append"
+								data-loading="none"
+							><i class="fa fa-fw fa-plus small"></i></a><?php
+						?></th><?php
+					endif;
+				?></tr>
+			</thead>
+		</table>
+	</header>
+	<fieldset><?php
+		// content
+		if ( !empty($fieldValue) ) :
+			foreach ( $fieldValue as $rowIndex => $rowItem ) :
+				include $fieldConfig['tableRow'];
+			endforeach;
+		endif;
+	?></fieldset>
+</section>
