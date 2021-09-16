3c047a7048f3611854701da96ac3f85e8398eaa2
henrygotmojo <henrygotmojo@gmail.com>
2021-09-16 10:39:04 +0800

    add more comments and change string-contains comparison to case-sensitive

--- app/model/Webform.php
+++ app/model/Webform.php
@@ -11,9 +11,6 @@ class Webform {
 		'uploadFile'     => __DIR__.'/../../lib/simple-ajax-uploader/2.6.7/extras/Uploader.php',
 		'uploadProgress' => __DIR__.'/../../lib/simple-ajax-uploader/2.6.7/extras/uploadProgress.php',
 	);
-
-
-
 	// get (latest) error message
 	private static $error;
 	public static function error() { return self::$error; }
@@ -24,12 +21,14 @@ class Webform {
 	/**
 	<fusedoc>
 		<description>
-			assertion methods for checking value against in-progress form data
+			check agaist in-progress form data
+			===> whether specific field contains certain string
+			===> find in array or find in string
 		</description>
 		<io>
 			<in>
-				<string name="$fieldName" comments="could be nested field name" example="first_name|student.name" />
-				<mixed name="$compareValue" />
+				<array_or_string name="$fieldName" comments="could be nested field name" example="first_name|student.name" />
+				<string name="$compareValue" />
 			</in>
 			<out>
 				<boolean name="~return~" />
@@ -42,11 +41,32 @@ class Webform {
 		if ( $formData === false ) throw new Exception(self::error());
 		$fieldValue = self::getNestedArrayValue($formData, $fieldName);
 		if ( is_array($fieldValue) ) return in_array($compareValue, $fieldValue);
-		return ( stripos($fieldValue, $compareValue) !== false );
+		return ( strpos($fieldValue, $compareValue) !== false );
 	}
 	public static function assertNotContains($fieldName, $compareValue) {
 		return !self::assertContains($fieldName, $compareValue);
 	}
+
+
+
+
+	/**
+	<fusedoc>
+		<description>
+			check against in-progress form data
+			===> whether specific field equals to certain value
+		</description>
+		<io>
+			<in>
+				<string name="$fieldName" comments="could be nested field name" example="first_name|student.name" />
+				<string name="$compareValue" />
+			</in>
+			<out>
+				<boolean name="~return~" />
+			</out>
+		</io>
+	</fusedoc>
+	*/
 	public static function assertEqual($fieldName, $compareValue) {
 		$formData = self::data();
 		if ( $formData === false ) throw new Exception(self::error());
