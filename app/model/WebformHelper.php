ab195bffb7e12e8b9076b733fc508c925c7b5522
henrygotmojo <henrygotmojo@gmail.com>
2021-09-29 14:37:57 +0800

    move assertion methods to WebformHelper

--- app/model/WebformHelper.php
+++ app/model/WebformHelper.php
@@ -17,4 +17,65 @@ class WebformHelper {
 	public static function error() { return self::$error; }
 
 
+
+
+	/**
+	<fusedoc>
+		<description>
+			check agaist in-progress form data
+			===> whether specific field contains certain string
+			===> find in array or find in string
+		</description>
+		<io>
+			<in>
+				<array_or_string name="$fieldName" comments="could be nested field name" example="first_name|student.name" />
+				<string name="$compareValue" />
+			</in>
+			<out>
+				<boolean name="~return~" />
+			</out>
+		</io>
+	</fusedoc>
+	*/
+	public static function assertContains($fieldName, $compareValue) {
+		$formData = self::data();
+		if ( $formData === false ) throw new Exception(self::error());
+		$fieldValue = self::getNestedArrayValue($formData, $fieldName);
+		if ( is_array($fieldValue) ) return in_array($compareValue, $fieldValue);
+		return ( strpos($fieldValue, $compareValue) !== false );
+	}
+	public static function assertNotContains($fieldName, $compareValue) {
+		return !self::assertContains($fieldName, $compareValue);
+	}
+
+
+
+
+	/**
+	<fusedoc>
+		<description>
+			check against in-progress form data
+			===> whether specific field equals to certain value
+		</description>
+		<io>
+			<in>
+				<string name="$fieldName" comments="could be nested field name" example="first_name|student.name" />
+				<string name="$compareValue" />
+			</in>
+			<out>
+				<boolean name="~return~" />
+			</out>
+		</io>
+	</fusedoc>
+	*/
+	public static function assertEqual($fieldName, $compareValue) {
+		$formData = self::data();
+		if ( $formData === false ) throw new Exception(self::error());
+		return ( self::getNestedArrayValue($formData, $fieldName) == $compareValue );
+	}
+	public static function assertNotEqual($fieldName, $compareValue) {
+		return !self::assertEqual($fieldName, $compareValue);
+	}
+
+
 } // class
