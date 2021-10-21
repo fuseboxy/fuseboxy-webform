4d50a91851e8b520e4aba161ffda46db4785f188
henrygotmojo <henrygotmojo@gmail.com>
2021-10-21 09:54:41 +0800

    display both success & warning messages

--- app/view/webform/completed.php
+++ app/view/webform/completed.php
@@ -2,14 +2,23 @@
 <fusedoc>
 	<io>
 		<in>
-			<structure name="$webform">
-				<structure name="customMessage">
-					<string name="completed" />
-				</structure>
+			<structure name="$result">
+				<string name="success" />
+				<array name="warning" optional="yes">
+					<string name="+" />
+				</array>
+				<number name="lastInsertID" />
 			</structure>
 		</in>
 		<out />
 	</io>
 </fusedoc>
 */ ?>
-<div id="webform-completed" class="alert alert-success"><?php echo $webform['customMessage']['completed']; ?></div>
+<div id="webform-completed"><?php
+	if ( !empty($result['success']) ) :
+		?><div class="alert alert-success"><?php echo $result['success']; ?></div><?php
+	endif;
+	if ( !empty($result['warning']) ) :
+		?><div class="alert alert-warning"><?php echo implode('<br />', $result['warning']); ?></div><?php
+	endif;
+?></div>
