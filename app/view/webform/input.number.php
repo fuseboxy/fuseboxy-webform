624284f6c24c7b5dbb3e304bc5fd941de804b9f4
henrygotmojo <henrygotmojo@gmail.com>
2022-03-29 16:48:19 +0800

    add input-number field type

---
+++ app/view/webform/input.number.php
@@ -0,0 +1,11 @@
+<?php
+// capture original output
+ob_start();
+include F::appPath('view/webform/input.text.php');
+$output = ob_get_clean();
+
+// replace class name
+$output = str_ireplace('webform-input-text', 'webform-input-number', $output);
+
+// display
+echo $output;
